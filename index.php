09ef7b01a2217ff4591783d633330424dc27d8a7
David Saunders <djsaun@gmail.com>
2014-02-10 21:31:25 -0600

    Added Github and Codepen accounts

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<title>David Saunders | Portfolio</title>
+	<meta name="description" content="David Saunders is a web developer living in Austin, TX.">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"> 
+	<link rel="stylesheet" href="css/styles.css" type="text/css" media="screen">
+	<link href='http://fonts.googleapis.com/css?family=Exo+2' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+
+<!-- Styles --> 
+	<link rel="stylesheet" href="css/normalize.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="css/styles.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="css/flexbox.css" type="text/css" media="screen">
+
+
+<!-- End styles -->
+
+</head>
+<body>
+	<div class="head">
+		<header>
+			<h1 class="headtext">Hello, I'm David Saunders.</h1>
+			<!-- <h1>I'm a ___</h1> -->
+		</header>
+		<section>
+			<nav class="container">
+				<ul class="nav">
+					<a href="#about"><li>About</li></a>
+					<a href="#work"><li>Work</li></a>
+					<a href="#contact"><li>Contact</li></a>
+					<a href="#"><li>Blog</li></a>
+				</ul>
+			</nav>
+		</section>
+	</div>
+		<article>
+				<div id="about">
+					<h3>About</h3>
+						<div class="image">
+							<img src="img/davidsaunders.png" alt="David Saunders" width="200" height="200" class="davidpic container">
+						</div>
+						<p>David Saunders is a web developer living in Austin, Texas, specializing in HTML5, CSS3, JavaScript/jQuery, and PHP. He currently works full-time as a technology project manager for Six Red Marbles, an educational publishing company. In his spare time, he designs and develops web sites, hikes, bikes, plays guitar, and does other fun things.</p>
+<!-- 						<div class="skills">
+							<ul>
+								<li>&#60;HTML&#62;</li>
+								<li>{CSS}</li>
+								<li>.javascript<li>
+								<li>&#60;? PHP ?&#62;</li>
+							</ul>
+					</div> -->
+				</div>	
+		</article>
+		<article>
+			<div id="work">
+				<h3>Work</h3>
+					<div class="samples">
+						<li class="thumbnail"></li>
+						<li class="thumbnail"></li>
+						<li class="thumbnail"></li>
+						<li class="thumbnail"></li>
+						<li class="thumbnail"></li>
+					</div>
+			</div>
+		</article>
+		<article>
+			<div id="contact">
+				<h3>Contact</h3>
+				<p>Get In Touch</p>
+				<ul class="logos">
+						<li><a href="https://github.com/djsaun" class="icon github" target="_blank" title="Github" width="54px" height="54px"></a></li>
+						<li><a href="https://www.codepen.io/djsaun" class="icon codepen" target="_blank" title="Codepen" width="54px" height="54px"></a></li>
+						<li><a href="https://www.facebook.com/dsaunders1000010101010101" class="icon facebook" target="_blank" title="Facebook" width="54px" height="54px"></a></li>
+						<li><a href="http://www.linkedin.com/profile/view?id=95648542" class="icon linkedin" target="_blank" title="LinkedIn" width="54px" height="54px"></a></li>
+<!-- 						<li><a href="https://plus.google.com/+DavidSaunders010100011" class="icon google" target="_blank" title="Google +" width="54px" height="54px"></a></li> -->
+						<li><a  href="http://open.spotify.com/user/djsaun" class="icon spotify" target="_blank" title="Spotify" width="54px" height="54px"></a></li>
+				</ul>
+				<form action="#" method="POST" name="contact_form" id="contactform">
+					<fieldset>
+						<div class="name">
+							<label for="Name">Name *</label>
+							<input type="text" name="Name" id="Name" pattern="[a-zA-Z ]+" required></input>
+						</div>
+						<div class="email">
+							<label for="Email">Email *</label>
+							<input type="email" Name="Email" id="Email" required></input>
+						</div>
+						<div class="message">
+							<label for="Message">Message</label>
+							<textarea name="Message" id="Message" rows="4" cols="50"></textarea>
+						</div>
+						<div class="submitbtn">
+							<input type="submit" value="Submit">
+						</div>
+					</fieldset>
+				</form>
+			</div>
+		</article>
+		<footer class="footer">
+			<p>&copy;<?php echo date('Y'); ?> David Saunders. All Rights Reserved.</p>
+		</footer>
+
+		</article>
+<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>		
+<script src="js/contact.js"></script>
+<script src="js/timeofday.js"></script>
+
+</body>
+</html>
